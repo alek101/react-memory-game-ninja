9a40408f45477cff430fe336057466b749c7ac30
alek101 <47416711+alek101@users.noreply.github.com>
2022-02-20 08:56:56 +0100

    singleCard

---
+++ src/components/SingleCard.js
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './SingleCard.css'
+
+function SingleCard({card}) {
+
+    return (
+        <div className="card" key={card.id}>
+            <div>
+                <img className="front" src={card.src} alt="card front" />
+                <img className="back" src="/img/cover.png" alt="card back" />
+            </div>
+        </div>
+    )
+}
+
+export default SingleCard
